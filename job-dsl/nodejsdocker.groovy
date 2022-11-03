899ad4838933677a3335aa3613b66befefed652b
Christopher Cupas <cupas.christopher@gmail.com>
2022-11-03 15:47:38 +0800

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/chriscupas/Offline-Events-Registration.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/chriscupas/node-docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
